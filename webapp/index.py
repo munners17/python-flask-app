fd652573d7fbe2cb1637082319baf5e7570eb6a1
Eric Z <ericzan7@gmail.com>
2020-03-04 17:25:29 -0800

    Create main flask stub

---
+++ webapp/index.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+   return "[CHANGE ME!]"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
